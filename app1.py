68e23f359e26a5ee875a02dc94ca2a63d67a2196
Pritam Dutta <duttapritam2222L@gmail.com>
2026-07-15 15:03:14 +0530

    CV_build

---
+++ app1.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title="CV BUILDER APP",
+    page_icon="👍",
+    layout="centered"
+)
+
+st.header("THIS IS THE APP POWERED BY GEMINI 3.5 TO YOUR CV")
